93cc2944fda22346c814cd836b1cfca1951c0d57
Bolortox <bolortogosboujee@gmail.com>
2024-10-23 08:38:20 +0800

    3rd page

--- frontend/src/components/page/Dashboard.jsx
+++ frontend/src/components/page/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Up } from "../svg/Up";
 
 export const Dashboard = () => {
   return (
-    <main className="w-screen h-screen m-auto bg-slate-200">
+    <main className="w-screen h-screen m-auto bg-slate-100 ">
       <div className="h-[72px] flex justify-between items-center px-28 bg-white">
         <div className="flex gap-4">
           <Geld />
@@ -15,17 +15,66 @@ export const Dashboard = () => {
           <div className="btn  bg-blue-500 text-white">+Record</div>
         </div>
       </div>
+
+      <div className="flex">
+        <div className="px-28 pt-10">
+          <div className=" bg-white w-[390px] h-[216px] rounded-2xl ">
+            <div className=" h-[56px] flex justify-start items-center border-b-2 border-b-slate-200 pl-5 gap-2">
+              <div className="w-2 h-2 rounded-full bg-lime-500"></div>
+              <p className="font-bold text-lg">Your Income</p>
+            </div>
+            <div className="pl-5">
+              <div className="pt-4">
+                <p className="font-bold text-4xl">1200247$</p>
+                <p className="text-gray-600">Your Income Amount</p>
+              </div>
+
+              <div className="flex pt-5">
+                <Up />
+                <p>32% from last month</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="px-28 pt-10">
+          <div className=" bg-white w-[390px] h-[216px] rounded-2xl ">
+            <div className=" h-[56px] flex justify-start items-center border-b-2 border-b-slate-200 pl-5 gap-2">
+              <div className="w-2 h-2 rounded-full bg-lime-500"></div>
+              <p className="font-bold text-lg">Your Income</p>
+            </div>
+            <div className="pl-5">
+              <div className="pt-4">
+                <p className="font-bold text-4xl">1200247$</p>
+                <p className="text-gray-600">Your Income Amount</p>
+              </div>
+
+              <div className="flex pt-5">
+                <Up />
+                <p>32% from last month</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="px-28 pt-10">
-        <div className=" bg-white w-[390px] h-[216px] rounded-2xl ">
-          <div className=" h-[56px] flex justify-start items-center border-b-2 border-b-slate-200 pl-5">
-            <p className="font-bold text-lg">Your Income</p>
+        <div className="max-w-screen-xl bg-white rounded-2xl">
+          <div className="font-bold text-lg h-[56px] pl-[24px]  flex items-center border-b-2 border-b-slate-300">
+            Last Records
           </div>
           <div>
-            <p className="text-gray-400">Your Income Amount</p>
-            <div>
-              <Up />
-              <p>percentage</p>
+            <div className="flex gap-3">
+              <p>Home</p>
+              <div>
+                <p className="font-bold">Lending & Renting</p>
+                <p className="text-slate-400">3 hours ago</p>
+              </div>
+              
             </div>
+            <div>
+                <p className="text-lime-500">- 1000$</p>
+                <div>DELETE</div>
+              </div>
           </div>
         </div>
       </div>
