055dfdcd88f810fef8939de6e7d939e16eebf2e5
Болортогос Баярбаатар <24LP6379@G401-CC0116.local>
2024-10-28 12:44:27 +0800

    made record add

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
+"use client";
 import { Geld } from "@/svg/Geld";
 import Link from "next/link";
+import { useState } from "react";
+import { AddRecord } from "./Record/AddRecord";
 
 export const Header = () => {
+  const [showAddRecord, setShowAddRecord] = useState(false);
+
+  const toggleAddRecord = () => {
+    setShowAddRecord((prev) => !prev);
+  };
   return (
     <main className="w-[1200px]">
       <div className="h-[72px] flex justify-between items-center bg-white">
@@ -17,7 +25,13 @@ export const Header = () => {
         </div>
 
         <div className="flex justify-center items-center gap-5 rounded-3xl">
-          <div className="btn  bg-blue-500 text-white">+Record</div>
+          <div
+            className="btn  bg-blue-500 text-white"
+            onClick={toggleAddRecord}
+          >
+            +Record
+          </div>
+          {showAddRecord && <AddRecord />}
           <div className="avatar">
             <div className="w-[40px] rounded">
               <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
--- frontend/src/components/Record/RecordsAdd.jsx
+++ frontend/src/components/Record/RecordsAdd.jsx
@@ -1,17 +1,31 @@
+"use client";
+
 import { Left } from "@/svg/Left";
 import { EyeSwitch } from "./EyeSwitch";
 import { Right } from "@/svg/Right";
 import { House } from "@/svg/House";
+import { useState } from "react";
+import { AddRecord } from "./AddRecord";
 
 export const RecordsAdd = () => {
+  const [showAddRecord, setShowAddRecord] = useState(false);
+
+  const toggleAddRecord = () => {
+    setShowAddRecord((prev) => !prev);
+  };
+
   return (
-    <div className="w-[1200px] flex gap-6">
+    <div className="w-[1200px] flex gap-6 relative">
       <div className="w-[282px]  h-[912px] px-[16px] py-[24px]  flex flex-col gap-6 border border-gray-200 rounded-2xl bg-white">
         <div className="flex flex-col gap-[24px]">
           <p className="font-bold text-2xl">Records</p>
-          <div className="btn bg-blue-500 text-white w-[250px] h-[28px] rounded-3xl text-lg border-none hover:bg-blue-400">
+          <button
+            onClick={toggleAddRecord}
+            className="btn bg-blue-500 text-white w-[250px] h-[28px] rounded-3xl text-lg border-none hover:bg-blue-400"
+          >
             + Add
-          </div>
+          </button>
+          {showAddRecord && <AddRecord />}
           <label className="input input-bordered flex items-center gap-2 h-[32px] bg-slate-100">
             <input type="text" className="grow" placeholder="Search" />
           </label>
@@ -106,6 +120,22 @@ export const RecordsAdd = () => {
                 </div>
               </div>
               <p className="font-bold text-green-500">+1000$</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <p className="font-bold text-lg pt-5 ">Yesterday</p>
+          <div className="flex flex-col gap-3">
+            <div className="w-[894px] h-[64px] bg-white flex justify-between items-center px-6 rounded-3xl  border border-gray-200 ">
+              <div className="flex gap-2 justify-center items-center">
+                <House />
+                <div className="flex flex-col ">
+                  <p className="font-medium">Lending & Renting</p>
+                  <p>14:00</p>
+                </div>
+              </div>
+              <p className="font-bold text-green-500">+2000$</p>
             </div>
           </div>
         </div>
