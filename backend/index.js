d9d19771dfda303afe0df08f49a941cd38576826
bolortox <bolortogosboujee@gmail.com>
2024-11-07 12:31:37 +0800

    11.06

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import cors from "cors;";
 
 dotenv.config();
 
@@ -13,19 +12,40 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const users = [
-  { name: "bolor", age: 19 },
-  { name: "togos", age: 22 },
+  { id: 1, name: "bolor", age: 19 },
+  { id: 2, name: "togos", age: 22 },
 ];
+
+const number = Math.random() * 100;
+const result = Math.floor(number);
 
 app.get("/", (request, response) => {
   response.send("GET huselt irlee");
 });
 
 app.post("/users", (request, response) => {
-  const newUsers = { name: request.body.name, age: request.body.age };
-  users.push(newUsers);
-  console.log(users);
-  response.send("POST huselt irlee");
+  try {
+    if (!request.body.name || !request.body.age) {
+      return response.send("NAME or AGE is not here");
+    } else {
+      if (typeof request.body.name !== "string") {
+        return response.send("NAME is not a string");
+      }
+      if (typeof request.body.age !== "number") {
+        return response.send("AGE is not a number");
+      }
+      const newUsers = {
+        id: request.body.id,
+        name: request.body.name,
+        age: request.body.age,
+      };
+      users.push(newUsers);
+      console.log(users);
+      response.json(users);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 app.listen(port, () => {
